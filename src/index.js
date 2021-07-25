a54a1cd577cec44591598ff6170cc8d59f59d0c8
jaeh <jascha@jaeh.at>
2021-07-25 23:42:10 +0200

    add src

---
+++ src/index.js
@@ -0,0 +1,54 @@
+export const map = () => {
+  console.log('shim.map called')
+}
+export const tileLayer = () => {
+  console.log('shim.tileLayer called')
+}
+export const Marker = () => {
+  console.log('shim.Marker called')
+}
+Marker.extend = () => console.log('shim.Marker.extend called')
+
+export const Util = () => {
+  console.log('shim.Util called')
+}
+export const divIcon = () => {
+  console.log('shim.divIcon called')
+}
+export const Control = () => {
+  console.log('shim.Control called')
+}
+Control.extend = () => console.log('shim.Control.extend called')
+
+export const DomUtil = () => {
+  console.log('shim.DomUtil called')
+}
+export const extend = () => {
+  console.log('shim.extend called')
+}
+export const LayerGroup = () => {
+  console.log('shim.LayerGroup called')
+}
+export const DomEvent = () => {
+  console.log('shim.DomEvent called')
+}
+export const circle = () => {
+  console.log('shim.circle called')
+}
+export const Icon = () => {
+  console.log('shim.Icon called')
+}
+Icon.extend = () => console.log('shim.Icon.extend called')
+
+export const popup = () => {
+  console.log('shim.popup called')
+}
+export const marker = () => {
+  console.log('shim.marker called')
+}
+export const FeatureGroup = () => {
+  console.log('shim.FeatureGroup called')
+}
+export const control = () => {
+  console.log('shim.control called')
+}
